134562da4195ed9f24e5dfe3effa183dcefacc0a
Build Service CI <>
2023-01-17 22:50:31 +0000

    Update Kpack CLI to 0.9.1

--- kp.rb
+++ kp.rb
@@ -4,14 +4,22 @@
   license "Apache-2.0"
 
   if OS.mac?
-    url "https://github.com/vmware-tanzu/kpack-cli/releases/download/v0.9.0/kp-darwin-amd64-0.9.0"
-    sha256 "0ba10edb3a360e51dde3d3120251af79927bef7834129cdb5f0f036b6d0b75d6"
+    if Hardware::CPU.intel?
+      url "https://github.com/vmware-tanzu/kpack-cli/releases/download/v0.9.1/kp-darwin-amd64-0.9.1"
+      sha256 "64abbec5d4a0f5763b604a3e80cd4210721983a6ff3ddbce5cff0a2edbb4e0b3"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/vmware-tanzu/kpack-cli/releases/download/v0.9.1/kp-darwin-arm64-0.9.1"
+      sha256 "224d8c95d8f8fb4ed950aec831d3c8dae2f404e3ce4dbf3b4378b1f7585f70ba"
+    end
   elsif OS.linux?
-    url "https://github.com/vmware-tanzu/kpack-cli/releases/download/v0.9.0/kp-linux-amd64-0.9.0"
-    sha256 "4853dac14686cf5d498fd226bcd0167d7a594cd9e0ce3a0092e307329215f850"
+    if Hardware::CPU.intel?
+      url "https://github.com/vmware-tanzu/kpack-cli/releases/download/v0.9.1/kp-linux-amd64-0.9.1"
+      sha256 "175d0818ed83e7d84f169dd74c26531d328cc0971937f2158a3adfefbcf57fdb"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/vmware-tanzu/kpack-cli/releases/download/v0.9.1/kp-linux-arm64-0.9.1"
+      sha256 "bc3034177cbf8a02e26ca0aa12d521efb5fb830d856448e9bfbb34992548fce6"
+    end
   end
-
-  depends_on :arch => :x86_64
 
   def install
     bin.install stable.url.split("/")[-1] => "kp"
